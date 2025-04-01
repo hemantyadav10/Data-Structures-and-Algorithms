34b9585f32d7e24cfa51a4f98dc7e35a76a59ec0
Hemant <yad.heman@gmail.com>
2025-04-01 23:39:29 +0530

    add comments to explain doubly linked list implementation

--- LinkedList/Doubly_Linked_List/doublyLinkedList.js
+++ LinkedList/Doubly_Linked_List/doublyLinkedList.js
@@ -1,24 +1,44 @@
+/**
+ * Doubly Linked List Implementation in JavaScript
+ * 
+ * A **Doubly Linked List (DLL)** is a linear data structure where each node 
+ * contains a value and pointers to both the next and previous nodes in the sequence.
+ * This allows for more efficient traversal in both directions compared to a Singly Linked List.
+ * 
+ * **Advantages:**
+ * - Dynamic size (no need to predefine capacity).
+ * - Efficient insertions and deletions at both the head and tail (O(1) time complexity).
+ * - Can be traversed in both directions (forward and backward).
+ * 
+ * **Disadvantages:**
+ * - Extra memory overhead due to two pointers (next and prev) in each node.
+ * - Requires additional operations to maintain both pointers when inserting or deleting nodes.
+ * 
+ * This implementation includes essential methods such as push, pop, shift, 
+ * unshift, and other common linked list operations.
+ */
 class Node {
   constructor(value) {
-    this.value = value;
-    this.next = null;
-    this.prev = null;
+    this.value = value; // The value stored in the node
+    this.next = null; // Pointer to the next node
+    this.prev = null; // Pointer to the previous node
   }
 }
 
 class DoublyLinkedList {
   constructor(value) {
     const newNode = new Node(value);
-    this.head = newNode;
-    this.tail = newNode;
-    this.length = 1;
+    this.head = newNode; // Create first node
+    this.tail = newNode; // Since there's only one node, tail = head
+    this.length = 1; // The list starts with 1 node
   }
 
+   /** Adds a node at the end of the list */
   push(value) {
     const newNode = new Node(value);
 
     if (!this.head) {
-      this.head = newNode;
+      this.head = newNode; 
       this.tail = newNode;
     } else {
       this.tail.next = newNode;
@@ -31,6 +51,7 @@ class DoublyLinkedList {
     return this;
   }
 
+  /** Removes and returns the last node */
   pop() {
     if (!this.head) return undefined;
 
@@ -50,6 +71,7 @@ class DoublyLinkedList {
     return temp;
   }
 
+   /** Adds a node at the beginning of the list */
   unshift(value) {
     const newNode = new Node(value);
 
@@ -67,6 +89,7 @@ class DoublyLinkedList {
     return this;
   }
 
+   /** Removes and returns the first node */
   shift() {
     if (this.length === 0) return undefined;
 
@@ -92,8 +115,6 @@ class DoublyLinkedList {
 const linkedList1 = new DoublyLinkedList(5);
 linkedList1.push(10);
 linkedList1.push(15);
-// linkedList1.push(20);
 
-
-console.log(linkedList1.shift())
+console.log(linkedList1.shift());
 console.log(linkedList1);
